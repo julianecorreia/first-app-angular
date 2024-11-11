b53ce8f4c11edc45bc573a6be75fb08ce889ca40
julianecorreia <juliane.correia@prof.unipar.br>
2024-11-11 19:33:29 -0300

    first commit

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,17 +1,22 @@
 import {Component, Input} from '@angular/core';
 import {TaskComponent} from './task/task.component';
+import {NewTaskComponent} from './new-task/new-task.component';
+import {NewTaskData} from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   imports: [
-    TaskComponent
+    TaskComponent,
+    NewTaskComponent
   ],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
+  @Input({required: true}) userId!: string;
   @Input({required: true}) name!: string;
+  isAddingTask = false;
 
   tasks = [
     {
@@ -39,4 +44,31 @@ export class TasksComponent {
     },
   ];
 
+  get selectedUserTasks() {
+    return this.tasks.filter(
+      task => task.userId === this.userId);
+  }
+
+  onCompleteTask(id: string) {
+    this.tasks = this.tasks.filter(task => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
+  }
+
+  onAddTask(taskData: NewTaskData) {
+    this.tasks.push({
+      id: Math.random().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.dueDate
+    })
+    this.isAddingTask = false;
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {CardComponent} from '../shared/card/card.component';
 
 interface User {
   id: string;
@@ -9,13 +10,15 @@ interface User {
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [
+    CardComponent
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
   @Input({required: true}) user!: User;
-
+  @Input({required: true}) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
   onSelectUser() {
